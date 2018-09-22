b18611363eb5b50ab46106b6ab19dd29ca784eda
Jędrek Domański <jedrek.domanski@gmail.com>
2018-09-22 18:04:25 +0200

    Mount API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+require './app/api/core'
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #API
+  mount API::Core => '/'
 end
