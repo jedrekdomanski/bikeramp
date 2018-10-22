5aebc214aeabaaffa7bcbdb5f95440300f6f4f1e
Jędrek Domański <jedrek.domanski@gmail.com>
2018-10-22 20:52:37 +0200

    Fix typoo in tests

--- spec/api/trips/trips_api_spec.rb
+++ spec/api/trips/trips_api_spec.rb
@@ -6,7 +6,7 @@ describe 'TripsAPI', type: :request do
     let(:destination_address) { 'Aleje Jerozolimskie 10, Warszawa' }
     let(:price) { '23.21' }
     let(:date) { '20.09.2018' }
-    let(:response_attribtues) { response.parsed_body }
+    let(:response_attributes) { response.parsed_body }
     subject { post '/api/trips', params: attributes }
 
     context 'with valid params' do
@@ -27,11 +27,11 @@ describe 'TripsAPI', type: :request do
       it 'creates a ride' do
         expect { subject }.to change { Ride.count }.from(0).to(1)
         expect(response).to be_successful
-        expect(response_attribtues['start_address']).to eq(start_address)
-        expect(response_attribtues['destination_address']).to eq(destination_address)
-        expect(response_attribtues['distance']).to eq(3)
-        expect(response_attribtues['price']).to eq(price)
-        expect(response_attribtues['date']).to eq('2018-09-20')
+        expect(response_attributes['start_address']).to eq(start_address)
+        expect(response_attributes['destination_address']).to eq(destination_address)
+        expect(response_attributes['distance']).to eq(2)
+        expect(response_attributes['price']).to eq(price)
+        expect(response_attributes['date']).to eq('2018-09-20')
       end
     end
 
