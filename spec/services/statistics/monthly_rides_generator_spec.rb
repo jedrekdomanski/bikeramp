ad677df94fc981c50c012a8b3006d02a4304169c
Jędrek Domański <jedrek.domanski@gmail.com>
2018-10-22 21:47:51 +0200

    Fix tests
    Used Timecop to create Rides with proper dates
    Calculations are now fine

--- spec/services/statistics/monthly_rides_generator_spec.rb
+++ spec/services/statistics/monthly_rides_generator_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 describe Statistics::MonthlyRidesGenerator, type: :service do
-  let(:date1) { '14.09.2018' }
-  let(:date2) { '12.09.2018' }
-  let(:date3) { '22.06.2018' }
+  before { Timecop.freeze(Date.new(2018, 10, 24)) }
+  after { Timecop.return }
 
   let!(:ride_same_day_1) do
     Ride.create!(
       start_address: 'Aleje Jerozolimskie 10, Warszawa',
       destination_address: 'Plac Europejski 1, Warszawa',
       price: 123.21,
-      date: date1,
+      date: Date.current,
       distance: 10
     )
   end
@@ -19,7 +18,7 @@ describe Statistics::MonthlyRidesGenerator, type: :service do
       start_address: 'Aleje Jerozolimskie 10, Warszawa',
       destination_address: 'Plac Europejski 1, Warszawa',
       price: 123.21,
-      date: date1,
+      date: Date.current,
       distance: 10
     )
   end
@@ -28,7 +27,7 @@ describe Statistics::MonthlyRidesGenerator, type: :service do
       start_address: 'Aleje Jerozolimskie 10, Warszawa',
       destination_address: 'Marii Grzegorzewskiej 4, Warszawa',
       price: 123.22,
-      date: date2,
+      date: 2.weeks.ago,
       distance: 15
     )
   end
@@ -37,7 +36,7 @@ describe Statistics::MonthlyRidesGenerator, type: :service do
       start_address: 'Aleje Jerozolimskie 10, Warszawa',
       destination_address: 'Marii Grzegorzewskiej 4, Warszawa',
       price: 123.22,
-      date: date2,
+      date: 2.weeks.ago,
       distance: 15
     )
   end
@@ -46,7 +45,7 @@ describe Statistics::MonthlyRidesGenerator, type: :service do
       start_address: 'Aleje Jerozolimskie 10, Warszawa',
       destination_address: 'Rosoła 45, Warszawa',
       price: 123.11,
-      date: date3,
+      date: 2.months.ago,
       distance: 15
     )
   end
@@ -63,13 +62,13 @@ describe Statistics::MonthlyRidesGenerator, type: :service do
     result = subject.call
     expect(result.size).to eq(2)
     expect(result.first).to eq(
-      day: 'September, 12th',
+      day: 'October, 10th',
       total_distance: '30km',
       avg_ride: '15km',
       avg_price: "#{avg_price_2}PLN"
     )
     expect(result.second).to eq(
-      day: 'September, 14th',
+      day: 'October, 24th',
       total_distance: '20km',
       avg_ride: '10km',
       avg_price: "#{avg_price_1}PLN"
