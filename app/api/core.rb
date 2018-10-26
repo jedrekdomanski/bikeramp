332bf407e46ec19a727de80e239da93768a89e97
Jędrek Domański <jedrek.domanski@gmail.com>
2018-10-26 12:09:51 +0200

    Add ErrHandlers, AuthHelpers and Users API to the base API

--- app/api/core.rb
+++ app/api/core.rb
@@ -1,10 +1,13 @@
 module API
   class Core < Grape::API
-    default_format :json
     prefix :api
+    include ErrorHandlers
+    helpers AuthHelpers
+    default_format :json
     content_type :json, 'application/json'
 
     mount ::Trips::Base
     mount ::Statistics::Base
+    mount ::Users::Base
   end
 end
