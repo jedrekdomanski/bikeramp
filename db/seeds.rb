3202eb14e252f1e5289025b2db4d910e356b8934
Jędrek Domański <jedrek.domanski@gmail.com>
2018-10-26 10:35:47 +0200

    Refactor seeds
    -add user to Ride
    -refactor to make it cleaner and read better

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,30 @@
 puts 'Seeding database...'
-
-Rides::CreateRide.new(
-  start_address: 'Aleje Jerozolimskie 10, Warszawa',
-  destination_address: 'Plac Zamkowy, Warszawa',
-  price: 23.99,
-  date: 1.day.ago
-).call
-
-Rides::CreateRide.new(
-  start_address: 'Marii Grzegorzewskiej 4, Warszawa',
-  destination_address: 'Plac Europejski 2, Warszawa',
-  price: 18.2,
-  date: 3.days.ago
-).call
-
-Rides::CreateRide.new(
-  start_address: 'Płaskowickiej 4, Warszawa',
-  destination_address: 'Rondo Waszyngtona, Warszawa',
-  price: 99.9,
-  date: 8.days.ago
-).call
-
-Rides::CreateRide.new(
-  start_address: 'Jana Rosoła 2, Warszawa',
-  destination_address: 'Aleja Krakowska 10, Warszawa',
-  price: 15.99,
-  date: 10.days.ago
-).call
-
-Rides::CreateRide.new(
-  start_address: 'Nowoursynowska 89, Warszawa',
-  destination_address: 'Adama Mickiewicza 4, Warszawa',
-  price: 78.29,
-  date: 20.days.ago
-).call
-
-Rides::CreateRide.new(
-  start_address: 'Racławicka 10, Warszawa',
-  destination_address: 'Batorego 2, Warszawa',
-  price: 61.11,
-  date: 21.days.ago
-).call
-
-Rides::CreateRide.new(
-  start_address: 'Koszykowa 7, Warszawa',
-  destination_address: 'Bracka 2, Warszawa',
-  price: 123.12,
-  date: 21.days.ago
-).call
+user1 = User.create(email: 'qwe@qwe.qwe', password: 'password', password_confirmation: 'password')
+user2 = User.create(email: 'some@email.com', password: 'password', password_confirmation: 'password')
+user3 = User.create(email: 'another_email@sample.com', password: 'password', password_confirmation: 'password')
+users = [user1, user2, user3]
+dates = [1.day.ago, 2.days.ago, 3.days.ago, 1.month.ago, 2.months.ago]
+start_addresses = [
+  'Aleje Jerozolimskie 10, Warszawa',
+  'Marii Grzegorzewskiej 4, Warszawa',
+  'Płaskowickiej 4, Warszawa',
+  'Jana Rosoła 2, Warszawa'
+]
+destination_addresses = [
+  'Plac Zamkowy, Warszawa',
+  'Plac Europejski 2, Warszawa',
+  'Rondo Waszyngtona, Warszawa',
+  'Aleja Krakowska 10, Warszawa'
+]
+prices = [23.99, 18.2, 99.9]
+params = {
+  start_address: start_addresses.sample,
+  destination_address: destination_addresses.sample,
+  price: prices.sample,
+  date: dates.sample
+}
+20.times do
+  Rides::CreateRide.new(params, users.sample).call
+end
 
 puts 'Seeds done.'
