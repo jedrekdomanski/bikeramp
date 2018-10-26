572629730b5c71053858c21b15eed56f670ab03f
Jędrek Domański <jedrek.domanski@gmail.com>
2018-10-26 11:43:12 +0200

    Add validations and scopes to Ride
    -validate the presence of user
    -add monthly and weekly scope

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,7 @@
 class Ride < ApplicationRecord
-  validates_presence_of :start_address, :destination_address, :price, :date
+  belongs_to :user
+  validates_presence_of :start_address, :destination_address, :price, :date, :user
+
+  scope :weekly, -> { where(date: Date.current.beginning_of_week..Date.current.end_of_week) }
+  scope :monthly, -> { where(date: Date.current.beginning_of_month..Date.current.end_of_month) }
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -5,19 +5,24 @@ describe Ride, type: :model do
   it { is_expected.to validate_presence_of(:destination_address) }
   it { is_expected.to validate_presence_of(:price) }
   it { is_expected.to validate_presence_of(:date) }
+  it { is_expected.to validate_presence_of(:user) }
+  it { is_expected.to belong_to(:user) }
 
   let(:start_address) { 'Plac Europejski 2, Warszawa, Polska' }
   let(:destination_address) { 'Marii Grzegorzewskiej 4, Warszawa, Polska' }
   let(:date) { Date.yesterday }
-
-  describe "price" do
+  let(:user) do
+      User.create(email: 'qwe@qwe.qwe', password: 'password', password_confirmation: 'password')
+    end
+  describe 'price' do
     let(:attributes) do
       {
         start_address: start_address,
         destination_address: destination_address,
         price: price,
         date: date,
-        distance: 120
+        distance: 120,
+        user: user
       }
     end
     
@@ -44,6 +49,62 @@ describe Ride, type: :model do
 
       it 'does not round the price' do
         expect(subject.price.to_f).to eq(29.0)
+      end
+    end
+  end
+
+  describe 'Scopes' do
+    let!(:ride_this_week1) do
+      Ride.create!(
+        date: Date.current,
+        user: user,
+        start_address: start_address,
+        destination_address: destination_address,
+        price: 29.67
+      )
+    end
+    let!(:ride_this_week2) do
+      Ride.create!(
+        date: Date.current,
+        user: user,
+        start_address: start_address,
+        destination_address: destination_address,
+        price: 29.67
+      )
+    end
+    let!(:ride_previous_week) do
+      Ride.create!(
+        date: 1.week.ago,
+        user: user,
+        start_address: start_address,
+        destination_address: destination_address,
+        price: 29.67
+      )
+    end
+    let!(:ride_previous_month) do
+      Ride.create!(
+        date: 1.months.ago,
+        user: user,
+        start_address: start_address,
+        destination_address: destination_address,
+        price: 29.67
+      )
+    end
+
+    before { Timecop.freeze(Time.new(2018, 10, 26, 10, 30).utc) }
+    after { Timecop.return }
+
+    describe '.weekly scope' do
+      it 'returns current week rides' do
+        expect(described_class.weekly).to include(ride_this_week1, ride_this_week2)
+        expect(described_class.weekly).not_to include(ride_previous_week)
+      end
+    end
+
+    describe '.monthly scope' do
+      it 'returns current month rides' do
+        expect(described_class.monthly).to include(ride_this_week1, ride_this_week2, ride_previous_week)
+        expect(described_class.monthly).not_to include(ride_previous_month)
       end
     end
   end
