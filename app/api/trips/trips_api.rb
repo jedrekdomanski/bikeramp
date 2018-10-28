15c094a5ea53c3177cef1e9c1323b56740bc8ba7
Jędrek Domański <jedrek.domanski@gmail.com>
2018-10-28 21:42:51 +0100

    Add new endpoints to TripsAPI
    -new endpoints GET /api/trips, PATCH /api/trips/:id, DELETE /api/trips/:id
    -tests

--- app/api/trips/trips_api.rb
+++ app/api/trips/trips_api.rb
@@ -9,6 +9,11 @@ module Trips
       end
     end
 
+    desc "Return all user's rides"
+    get do
+      current_user.rides.order(date: 'asc')
+    end
+
     desc 'Create new ride'
     params do
       use :trips_params
@@ -17,10 +22,30 @@ module Trips
     post do
       result = Rides::CreateRide.new(params, current_user).call
       if result.success?
-        status :ok
+        { data: result.data }
       else
         error!({ message: result.message }, 403)
       end
+    end
+
+    desc "Updates user's ride"
+    params do
+      optional :start_address, type: String
+      optional :destination_address, type: String
+      optional :price, type: Float
+      optional :date, type: Date
+    end
+    patch '/:id' do
+      ride = current_user.rides.find(params[:id])
+      ride.update(params)
+      body false
+    end
+
+    desc "Deletes user's ride"
+    delete ':id' do
+      ride = current_user.rides.find(params[:id])
+      ride.destroy
+      body false
     end
   end
 end
--- spec/api/trips/trips_api_spec.rb
+++ spec/api/trips/trips_api_spec.rb
@@ -1,16 +1,72 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe 'TripsAPI', type: :request do
+  let(:user) do
+    User.create(
+      email: 'email@mail.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+  end
   describe 'POST /api/trips' do
     let(:start_address) { 'Plac Europejski 2, Warszawa' }
     let(:destination_address) { 'Aleje Jerozolimskie 10, Warszawa' }
     let(:price) { '23.21' }
     let(:date) { '20.09.2018' }
     let(:response_attributes) { response_body }
-    let(:user) { User.create(email: 'email@mail.com', password: 'password', password_confirmation: 'password') }
-    subject { post '/api/trips', params: attributes, headers: auth_headers(user) }
+    subject { post '/api/trips', params: attributes, headers: headers }
 
-    context 'with valid params' do
+    context 'when user is authenticated' do
+      let(:headers) { auth_headers(user) }
+
+      context 'with valid params' do
+        let(:attributes) do
+          {
+            start_address: start_address,
+            destination_address: destination_address,
+            price: price,
+            date: date
+          }
+        end
+
+        include_examples 'responds with JSON'
+        include_examples '201'
+
+        it 'creates a ride' do
+          expect { subject }.to change(Ride, :count).by(1)
+        end
+      end
+
+      shared_examples 'does not create a new ride' do
+        it 'does not create a new ride' do
+          expect { subject }.not_to change(Ride, :count)
+          expect(response.status).to eq(406)
+        end
+      end
+
+      context 'with invalid params' do
+        context 'with empty params' do
+          let(:attributes) { {} }
+
+          it_behaves_like 'does not create a new ride'
+        end
+        context 'with invalid price and date' do
+          let(:attributes) do
+            {
+              start_address: 123,
+              destination_address: 123,
+              price: 'John Doe',
+              date: 'ABCDEFGH'
+            }
+          end
+
+          it_behaves_like 'does not create a new ride'
+        end
+      end
+    end
+    context 'when user is not authenticated' do
       let(:attributes) do
         {
           start_address: start_address,
@@ -19,43 +75,132 @@ describe 'TripsAPI', type: :request do
           date: date
         }
       end
+      include_examples 'Unauthenticated' 
+    end
+  end
 
-      it 'responds with JSON' do
+  describe 'GET /api/trips' do
+
+    subject { get '/api/trips', headers: headers }
+    
+    context 'when user is authenticated' do
+      let!(:ride1) do
+        Ride.create(
+          start_address: 'Plac Europejski 2, Warszawa',
+          destination_address: 'Aleje Jerozolimskie 10, Warszawa',
+          price: 100,
+          date: '2018-10-20',
+          user: user
+        )
+      end
+      let!(:ride2) do
+        Ride.create(
+          start_address: 'Plac Europejski 2, Warszawa',
+          destination_address: 'Aleje Jerozolimskie 10, Warszawa',
+          price: 80,
+          date: '2018-10-10',
+          user: user
+        )
+      end
+      let(:headers) { auth_headers(user) }
+      
+      include_examples '200'
+      include_examples 'responds with JSON'
+      
+      it "returns user's rides ordered by date ascending" do
         subject
-        expect(response.content_type).to eq('application/json')
-      end
-
-      it 'creates a ride' do
-        expect { subject }.to change { Ride.count }.from(0).to(1)
-        expect(response.status).to eq(200)
+        expect(response_body).to be_kind_of(Array)
+        expect(response_body.first['date']).to eq('2018-10-10')
+        expect(response_body.second['date']).to eq('2018-10-20')
       end
     end
 
-    shared_examples 'does not create a new ride' do
-      it 'does not create a new ride' do
-        expect { subject }.not_to change { Ride.count }
-        expect(response.status).to eq(406)
-      end
+    context 'when user is not authenticated' do
+      include_examples 'Unauthenticated' 
+    end
+  end
+
+  describe 'PATCH /api/trips/:id' do
+    let(:ride) do
+      Ride.create(
+        start_address: 'Plac Europejski 2, Warszawa',
+        destination_address: 'Aleje Jerozolimskie 10, Warszawa',
+        price: 80,
+        date: '2018-10-10',
+        user: user
+      )
+    end
+  
+    let(:params) do
+      {
+        id: id,
+        start_address: 'Piękna 5, Łódź',
+        destination_address: 'Brzydka 666, Łódź'
+      }
     end
 
-    context 'with invalid params' do
-      context 'with empty params' do
-        let(:attributes) { {} }
+    subject { patch "/api/trips/#{id}", headers: headers, params: params }
 
-        it_behaves_like 'does not create a new ride'
-      end
-      context 'with invalid price and date' do
-        let(:attributes) do
-          {
-            start_address: 123,
-            destination_address: 123,
-            price: 'John Doe',
-            date: 'ABCDEFGH'
-          }
+    context 'when user is authenticated' do
+      let(:headers) { auth_headers(user) }
+
+      context 'when the ride exists ' do
+        let(:id) { ride.id }
+        
+        include_examples '204'
+        
+        it 'updates the rides with attribtues set in params' do
+          subject
+          updated_ride = ride.reload
+          expect(updated_ride.start_address).to eq(params[:start_address])
+          expect(updated_ride.destination_address).to eq(params[:destination_address])
         end
+       
+        context 'when ride does not exist' do
+          let(:id) { not_existing_id(Ride) }
 
-        it_behaves_like 'does not create a new ride'
+          include_examples '404'
+        end
       end
+    end
+
+    context 'when user is not authenticated' do
+      let(:id) { ride.id }
+
+      include_examples 'Unauthenticated'
+    end
+  end
+
+  describe 'DELETE /api/trips/:id' do
+    subject { delete "/api/trips/#{id}", headers: headers }
+
+    context 'when user is authenticated' do
+      let(:headers) { auth_headers(user) }
+
+      context 'when ride exists' do
+        let(:ride) do
+          Ride.create(
+            start_address: 'Plac Europejski 2, Warszawa',
+            destination_address: 'Aleje Jerozolimskie 10, Warszawa',
+            price: 80,
+            date: '2018-10-10',
+            user: user
+          )
+        end
+        let(:id) { ride.id }
+
+        include_examples '204'
+      end
+
+      context 'when the ride does not exist' do
+        let(:id) { not_existing_id(Ride) }
+
+        include_examples '404'
+      end
+    end
+
+    context 'when user is not authenticated' do
+      let(:id) { ride.id }
     end
   end
 end
