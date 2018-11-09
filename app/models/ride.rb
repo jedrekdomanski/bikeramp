ab2c6bb5b6488dc7b26a9f9fd04b228adadfed52
Jędrek Domański <jedrek.domanski@gmail.com>
2018-11-09 19:37:32 +0100

    Overwrite as_json method to return price as decimal
    - by default price is returned as string, so had to overwrite this
    method to return decimal needed for the front end calculations

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,4 +4,15 @@ class Ride < ApplicationRecord
 
   scope :weekly, -> { where(date: Date.current.beginning_of_week..Date.current.end_of_week) }
   scope :monthly, -> { where(date: Date.current.beginning_of_month..Date.current.end_of_month) }
+
+  def as_json(options)
+    {
+      id: self.id,
+      start_address: self.start_address,
+      destination_address: self.destination_address,
+      distance: self.distance,
+      price: self.price.to_f,
+      date: self.date
+    }
+  end
 end
