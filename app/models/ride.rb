573667650ae21f4554386fdbcf569b3b3e93d56d
Jędrek Domański <jedrek.domanski@gmail.com>
2018-11-19 10:01:06 +0100

    Validate the presence of price_cents

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,19 +1,9 @@
 class Ride < ApplicationRecord
   belongs_to :user
-  validates_presence_of :start_address, :destination_address, :price, :date, :user
+  validates_presence_of :start_address, :destination_address, :price_cents, :date, :user
+
+  monetize :price_cents
 
   scope :weekly, -> { where(date: Date.current.beginning_of_week..Date.current.end_of_week) }
   scope :monthly, -> { where(date: Date.current.beginning_of_month..Date.current.end_of_month) }
-
-  def as_json(options)
-    {
-      id: self.id,
-      start_address: self.start_address,
-      destination_address: self.destination_address,
-      distance: self.distance,
-      price: self.price.to_f,
-      date: self.date,
-      user_id: self.user.id
-    }
-  end
 end
