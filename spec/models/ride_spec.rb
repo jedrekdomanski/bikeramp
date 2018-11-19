942f39476f4f92db355b69746e2b39c179694404
Jędrek Domański <jedrek.domanski@gmail.com>
2018-11-19 10:22:35 +0100

    Validate price_cents and monetization of price
    - remove price precision validations as precision is already handled by
    money-rails

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 describe Ride, type: :model do
   it { is_expected.to validate_presence_of(:start_address) }
   it { is_expected.to validate_presence_of(:destination_address) }
-  it { is_expected.to validate_presence_of(:price) }
+  it { is_expected.to validate_presence_of(:price_cents) }
   it { is_expected.to validate_presence_of(:date) }
   it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to monetize(:price_cents) }
 
   let(:start_address) { 'Plac Europejski 2, Warszawa, Polska' }
   let(:destination_address) { 'Marii Grzegorzewskiej 4, Warszawa, Polska' }
@@ -14,44 +15,6 @@ describe Ride, type: :model do
   let(:user) do
       User.create(email: 'qwe@qwe.qwe', password: 'password', password_confirmation: 'password')
     end
-  describe 'price' do
-    let(:attributes) do
-      {
-        start_address: start_address,
-        destination_address: destination_address,
-        price: price,
-        date: date,
-        distance: 120,
-        user: user
-      }
-    end
-    
-    subject { described_class.create!(attributes) }
-
-    context 'with more than 2 digits after period' do
-      let(:price) { 29.6786745 }
-
-      it 'rounds the price to 2 digits after period' do
-        expect(subject.price.to_f).to eq(29.68)
-      end
-    end
-
-    context 'with 2 digits after period' do
-      let(:price) { 29.67 }
-
-      it 'does not round the price' do
-        expect(subject.price.to_f).to eq(29.67)
-      end
-    end
-
-    context 'with no digits after period' do
-      let(:price) { 29 }
-
-      it 'does not round the price' do
-        expect(subject.price.to_f).to eq(29.0)
-      end
-    end
-  end
 
   describe 'Scopes' do
     before { Timecop.freeze(Time.new(2018, 10, 26, 10, 30).utc) }
