f036d94cd8fb168e106ad30d826e484d06b250ac
Jędrek Domański <jedrek.domanski@gmail.com>
2018-12-11 16:55:35 +0100

    Test user for :avatar db column

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,5 @@ describe User, type: :model do
   it { is_expected.to have_many(:rides) }
   it { is_expected.to have_many(:weekly_rides) }
   it { is_expected.to have_many(:monthly_rides) }
+  it { is_expected.to have_db_column(:avatar)}
 end
