f307f655644111c003492a62ec9f291a6cbae02d
Jędrek Domański <jedrek.domanski@gmail.com>
2018-12-11 16:54:11 +0100

    Add user factories

---
+++ spec/factories/user.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    sequence :email do |n|
+      "#{n}-#{Faker::Internet.email}"
+    end
+    password "Password"
+    password_confirmation "Password"
+  end
+
+  trait :with_avatar do
+    avatar File.open('spec/fixtures/files/jpg.jpg')
+  end
+end
