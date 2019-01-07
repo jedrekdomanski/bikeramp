a5ff6a27757a479e9161453fc28018c57f2f0523
Jędrek Domański <jedrek.domanski@gmail.com>
2019-01-07 12:57:56 +0100

    Add default format to Money

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,6 +1,10 @@
 MoneyRails.configure do |config|
   # set the default currency
   config.default_currency = :pln
+  config.default_format = {
+    no_cents_if_whole: nil,
+    symbol: 'PLN'
+  }
 end
 
 Money.locale_backend = :i18n
