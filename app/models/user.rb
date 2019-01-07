87af93130a4fe0fe2437e6724783d8ae7fb2e202
Jędrek Domański <jedrek.domanski@gmail.com>
2019-01-07 12:48:52 +0100

    Remove and update validations
    -remove password validations, it's already included in Devise gem
    -add phone number validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
@@ -10,12 +12,12 @@ class User < ApplicationRecord
   has_many :weekly_rides, -> { weekly }, class_name: 'Ride', inverse_of: :user
   has_many :monthly_rides, -> { monthly }, class_name: 'Ride', inverse_of: :user
   has_many :invoices
+  validates :email, :first_name, :last_name, presence: true
   validates(
-    :email, :password, :password_confirmation, :first_name, :last_name,
-    presence: true
+    :phone_number,
+    numericality: { only_integer: true }, allow_nil: true, on: :update
   )
-
-  validates :password, length: { minimum: 6 }
+  validates_length_of :phone_number, is: 9, allow_nil: true, on: :update
 
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
