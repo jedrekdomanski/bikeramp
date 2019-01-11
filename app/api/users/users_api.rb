c164156e3103df2ae13fe6fd6401d525af560ab0
Jędrek Domański <jedrek.domanski@gmail.com>
2019-01-11 11:08:47 +0100

    Refactor UsersAPI

--- app/api/users/users_api.rb
+++ app/api/users/users_api.rb
@@ -1,13 +1,23 @@
 module Users
   class UsersAPI < Base
+    helpers do
+      params :user_params do
+        optional :first_name, type: String
+        optional :last_name, type: String
+        optional :avatar, type: File
+        optional :hourly_rate, type: String
+        optional :phone_number, type: Integer
+        optional :facebook_url, type: String
+        optional :twitter_url, type: String
+        optional :linked_in_url, type: String
+      end
+    end
+    
     desc 'Update the user'
     params do
-      optional :first_name, type: String
-      optional :last_name, type: String
-      optional :avatar, type: File
-      optional :password, type: String
-      optional :password_confirmation, type: String
+      use :user_params
     end
+
     patch '/:id' do
       user = User.find(params[:id])
       user.update(params)
