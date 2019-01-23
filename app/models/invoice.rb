ae09b9d37b0756cb2cdf7fc2149ae3893236549a
Jędrek Domański <jedrek.domanski@gmail.com>
2019-01-23 13:04:22 +0100

    Remove redundant  association

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,6 @@
 class Invoice < ApplicationRecord
   belongs_to :user
   has_many :invoice_line_items
-  has_one :invoice_file
 
   mount_uploader :file, InvoiceFileUploader
   monetize :total_net_amount_cents
