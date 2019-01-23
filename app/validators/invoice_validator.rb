fe9d0b88a477de6844b0cb49db515d87478760ef
Jędrek Domański <jedrek.domanski@gmail.com>
2019-01-23 13:03:33 +0100

    Validate _amounts_cents in InvoiceValidator

--- app/validators/invoice_validator.rb
+++ app/validators/invoice_validator.rb
@@ -9,13 +9,13 @@ class InvoiceValidator
   attribute :sale_date, Date
   attribute :currency, String
   attribute :payment_method, String
-  attribute :total_net_amount, Float
-  attribute :total_gross_amount, Float
+  attribute :total_net_amount_cents, Float
+  attribute :total_gross_amount_cents, Float
   attribute :invoice_line_items, Array
 
   validates(
     :number, :creation_date, :sale_date, :currency,
-    :payment_method, :total_net_amount, :total_gross_amount,
+    :payment_method, :total_net_amount_cents, :total_gross_amount_cents,
     :invoice_line_items, presence: true
   )
 end
